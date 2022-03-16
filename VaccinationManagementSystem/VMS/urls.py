4586bb39edc464c49e08a94074c54a3f973d8304
Amal-droid <91870514+Amal-droid@users.noreply.github.com>
2022-03-16 13:46:45 +0530

    commit

--- VaccinationManagementSystem/VMS/urls.py
+++ VaccinationManagementSystem/VMS/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.staticfiles.views import serve
-from django.template.defaulttags import url
+
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('vms_app.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
